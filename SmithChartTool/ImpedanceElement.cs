df0446da9cafbb24f818ff209114eff688f363fc
bonbytes <58460227+bonbytes@users.noreply.github.com>
2020-01-16 00:21:07 +0100

    initial commit (with sources)

---
+++ SmithChartTool/ImpedanceElement.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MathNet.Numerics;
+
+
+namespace SmithChartTool
+{
+    class ImpedanceElement
+    {
+        private MathNet.Numerics.Complex32 _impedance;
+        private string _portName;
+        private bool _normalized;
+
+        public ImpedanceElement(MathNet.Numerics.Complex32 impedance)
+        {
+            this.Impedance = impedance;
+        }
+
+        public MathNet.Numerics.Complex32 Impedance
+        {
+            get 
+            {
+                return this._impedance;
+            }
+            set
+            {
+                if(value.Real >= 0)
+                {
+                    this._impedance = value;
+                }
+                else
+                    throw new Exception("Given impedance has negative real part.");
+            }
+        }
+
+        public string PortName
+        {
+            get
+            {
+                return this._portName;
+            }
+            set
+            {
+                this._portName = value;
+            }
+        }
+
+    }
+}
