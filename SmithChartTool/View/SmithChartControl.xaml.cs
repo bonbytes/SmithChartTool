6f79ecc09b45409d329b6333239ecba21fd62949
bonbytes <58460227+bonbytes@users.noreply.github.com>
2020-01-31 07:17:24 +0100

    - updates on SmithChart drawing

--- SmithChartTool/View/SmithChartControl.xaml.cs
+++ SmithChartTool/View/SmithChartControl.xaml.cs
@@ -20,30 +20,7 @@ namespace SmithChartTool.View
 	/// </summary>
 	public partial class UserControl1 : UserControl
 	{
-		/*
-		 *  // konforme Abbdildung in Matlab / Octave
-		    f = @(x) (x - 1) ./ (x + 1);
-
-			close all;
-			hold on;
-
-			zero_line = 1E-20
-
-			jrange = logspace(log10(0.15), log10(100), 10)
-
-			# imag. const
-			for jmul = [-jrange zero_line jrange]
-				plot(f((0:0.1:100)+(j*jmul)))
-			endfor
-
-			# real. const
-			for roffs = [0 0.2 0.5 1 2 5]
-				plot(f(roffs + j*(-100:0.05:100)))
-			endfor
-		 * 
-		 */
-
-		public UserControl1()
+			public UserControl1()
 		{
 			InitializeComponent();
 		}
--- SmithChartTool/ViewModel/MainWindowViewModel.cs
+++ SmithChartTool/ViewModel/MainWindowViewModel.cs
@@ -11,13 +11,79 @@ namespace SmithChartTool.ViewModel
 {
     public class MainWindowViewModel
     {
+    
+        /*
+        // konforme Abbdildung in Matlab / Octave
+        f = @(x) (x - 1) ./ (x + 1);
+
+        close all;
+        hold on;
+
+        zero_line = 1E-20
+
+        jrange = logspace(log10(0.15), log10(100), 10)
+
+        # imag. const
+        for jmul = [-jrange zero_line jrange]
+            plot(f((0:0.1:100)+(j*jmul)))
+        endfor
+
+        # real. const
+        for roffs = [0 0.2 0.5 1 2 5]
+            plot(f(roffs + j*(-100:0.05:100)))
+        endfor 
+        */
+
         public PlotModel MyFirstPlot { get; private set; }
-        
+
+        private MathNet.Numerics.Complex32 getConformalValue(MathNet.Numerics.Complex32 z)
+        {
+            return ((z - 1) / (z + 1));
+        }
+
+        public FunctionSeries DrawSmithChart()
+        {
+            List<double> x = new List<double>(new IEnumerable<double>());
+            double i = 0;
+            
+            while(i <= 100)
+            {
+                x.Add(i);
+                i = i + 0.1;
+            }
+
+
+            FunctionSeries serie = new FunctionSeries();
+
+            for (int x = 0; x < n; x++)
+            {
+                for (int y = 0; y < n; y++)
+                {
+                    //adding the points based x,y
+                    DataPoint data = new DataPoint(x, getValue(x, y));
+
+                    //adding the point to the serie
+                    serie.Points.Add(data);
+                }
+            }
+            //returning the serie
+            return serie;
+        }
 
         public MainWindowViewModel()
         {
-            this.MyFirstPlot = new PlotModel { Title = "Plot" };
+            this.MyFirstPlot = new PlotModel();
             this.MyFirstPlot.Series.Add(new FunctionSeries(Math.Cos, 0, 10, 0.1, "cos(x)"));
+            MyFirstPlot.LegendPosition = LegendPosition.RightBottom;
+            MyFirstPlot.LegendPlacement = LegendPlacement.Outside;
+            MyFirstPlot.LegendOrientation = LegendOrientation.Horizontal;
+            var Yaxis = new OxyPlot.Axes.LinearAxis();
+            OxyPlot.Axes.LinearAxis XAxis = new OxyPlot.Axes.LinearAxis { Position = OxyPlot.Axes.AxisPosition.Bottom, Minimum = -1, Maximum = 1 };
+            XAxis.Title = "Real";
+            Yaxis.Title = "Imaginary";
+            MyFirstPlot.Axes.Add(Yaxis);
+            MyFirstPlot.Axes.Add(XAxis);
+            //this.plot.Model = MyFirstPlot;
         }
 
         public static RoutedUICommand CommandXYAsync = new RoutedUICommand("Run XY Async", "RXYA", typeof(MainWindowView), new InputGestureCollection() { new KeyGesture(Key.F5), new KeyGesture(Key.R, ModifierKeys.Control) });
